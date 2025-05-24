342a40e64927e69619b51c3f2e3946e7db80bb7c
alex.aguirre@culqi.com <alex.aguirre@culqi.com>
2025-05-24 09:39:07 -0500

    removed unnecessary recursive calls

--- src/numberwriter.py
+++ src/numberwriter.py
@@ -1,9 +1,13 @@
 import re
 import random
+import time
 
 
 class NumberToWords:
     def __init__(self):
+        self.thousandsInfix = " MIL "
+        self.millionInfix = " MILLON "
+        self.millionsInfix = " MILLONES "
         self.moneda = "SOLES"
         self.quincenas = [
             "",
@@ -87,7 +91,7 @@ class NumberToWords:
             raise ValueError("El número no es válido")
 
         prefix = self.get_prefix(number)
-        result = prefix + self.convertir(abs(number))
+        result = prefix + self.convertir(abs(number), True)
         return self.clean_spaces(result)
 
     def get_suffix_and_currency(self, number):
@@ -95,29 +99,27 @@ class NumberToWords:
         number = int(number * 100)
         return f" Y {number:02}/100 {self.moneda}"
 
-    def convertir(self, number):
+    def convertir(self, number, top=False):
         if number < 1:
             return self.get_suffix_and_currency(number)
         elif 0 < number < 16:
             return self.unidades(number)
         elif number < 101:
-            return self.diesmos(1, number)
+            return self.diesmos(number)
         elif number < 1001:
-            return self.cientos(1, number)
+            return self.cientos(number)
         elif number < 16000:
-            return self.miles(1, number)
+            return self.miles(number, top)
         elif number < 101000:
-            return self.diezmiles(1, number)
+            return self.diezmiles(number)
         elif number < 1000000:
-            return self.cienmiles(1, number)
+            return self.cienmiles(number)
         elif number < 16000000:
-            return self.millones(1, number)
-        elif number < 1000000000:
-            raise ValueError(
-                "El número es demasiado grande. El límite es 15,999,999.99"
-            )
+            return self.millones(number, top)
+        elif number < 101000000:
+            return self.diezmillones(number)
 
-        return False
+        return ""
 
     def unidades(self, number):
         if int(number) == 1:
@@ -126,83 +128,69 @@ class NumberToWords:
             prefix = self.quincenas[int(number)]
         return prefix + self.convertir(number - int(number))
 
-    def diesmos(self, index, number):
-        if index < 11:
-            next_upper_threshold = (index + 1) * 10
-            if next_upper_threshold > number:
-                if index * 10 != int(number):
-                    prefix = self.decenarios[index]
-                else:
-                    prefix = self.decenas[index]
-                return prefix + self.convertir(10 - (next_upper_threshold - number))
-            return self.diesmos(index + 1, number)
-        return ""
+    def diesmos(self, number):
+        index = int(number / 10)
+        remainder = number % 10
+        if int(remainder) == 0:
+            prefix = self.decenas[index]
+        else:
+            prefix = self.decenarios[index]
+        return prefix + self.convertir(remainder)
 
-    def cientos(self, index, number):
-        if index < 11:
-            next_upper_threshold = (index + 1) * 100
-            if next_upper_threshold > number:
-                return self.centenas[index] + self.convertir(
-                    100 - (next_upper_threshold - number)
-                )
-            return self.cientos(index + 1, number)
-        return ""
+    def cientos(self, number):
+        index = int(number / 100)
+        remainder = number % 100
+        prefix = self.centenas[index]
+        return prefix + self.convertir(remainder)
 
-    def miles(self, index, number):
-        if index < 16:
-            next_upper_threshold = (index + 1) * 1000
-            if next_upper_threshold > number:
-                return (
-                    self.quincenas[index]
-                    + " MIL "
-                    + self.convertir(1000 - (next_upper_threshold - number))
-                )
-            return self.miles(index + 1, number)
-        return ""
+    def miles(self, number, top):
+        index = int(number / 1000)
+        remainder = number % 1000
+        if index == 1 and top:
+            index = 0
+        prefix = self.quincenas[index] + self.thousandsInfix
+        return prefix + self.convertir(remainder)
 
-    def diezmiles(self, index, number):
-        if index < 11:
-            next_upper_threshold = (index + 1) * 10000
-            if next_upper_threshold > number:
-                if number - (index * 10000) >= 1000:
-                    prefix = self.decenarios[index]
-                else:
-                    prefix = self.decenas[index] + " MIL "
-                return prefix + self.convertir(10000 - (next_upper_threshold - number))
-            return self.diezmiles(index + 1, number)
-        return ""
+    def diezmiles(self, number):
+        index = int(number / 10000)
+        remainder = number % 10000
+        if int(remainder / 1000) == 0:
+            prefix = self.decenas[index] + self.thousandsInfix
+        else:
+            prefix = self.decenarios[index]
+        return prefix + self.convertir(remainder)
 
-    def cienmiles(self, index, number):
-        if index < 11:
-            next_upper_threshold = (index + 1) * 100000
-            if next_upper_threshold > number:
-                if number - (index * 100000) < 1000:
-                    prefix = self.centenas[index] + " MIL "
-                else:
-                    prefix = self.centenas[index]
-                return prefix + self.convertir(100000 - (next_upper_threshold - number))
-            return self.cienmiles(index + 1, number)
-        return ""
+    def cienmiles(self, number):
+        index = int(number / 100000)
+        remainder = number % 100000
+        prefix = self.centenas[index]
+        if int(remainder / 1000) == 0:
+            prefix += self.thousandsInfix
+        return prefix + self.convertir(remainder)
 
-    def millones(self, index, number):
-        if index < 16:
-            next_upper_threshold = (index + 1) * 1000000
-            if next_upper_threshold > number:
-                if index == 1:
-                    infix = " MILLON "
-                else:
-                    infix = " MILLONES "
-                return (
-                    self.quincenas[index]
-                    + infix
-                    + self.convertir(1000000 - (next_upper_threshold - number))
-                )
+    def millones(self, number, top):
+        index = int(number / 1000000)
+        remainder = number % 1000000
+        if index == 1 and top:
+            infix = self.millionInfix
+        else:
+            infix = self.millionsInfix
 
-            return self.millones(index + 1, number)
-        return ""
+        prefix = self.quincenas[index] + infix
+        return prefix + self.convertir(remainder)
+
+    def diezmillones(self, number):
+        index = int(number / 10000000)
+        remainder = number % 10000000
+        if int(remainder / 1000000) == 0:
+            prefix = self.decenas[index] + self.millionsInfix
+        else:
+            prefix = self.decenarios[index]
+        return prefix + self.convertir(remainder)
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     number_to_words = NumberToWords()
     number = round(random.uniform(0, 100), 2)
     print(f"Random number: {number}")
@@ -225,3 +213,7 @@ if __name__ == "__main__":
     number = round(random.uniform(10000001, 15999999), 2)
     print(f"Random number: {number}")
     print(f"Number in words: {number_to_words.convert(number)}")
+    number = round(random.uniform(16000000, 100999999), 2)
+    print(f"Random number: {number}")
+    print(f"Number in words: {number_to_words.convert(number)}")
+    print(f"Execution time: {time.time() - start_time}")
